b06e2634e89018a019f380e9ea9bd9a86ed22066
Shraya Jaiswal <shrayajaiswal1@gmail.com>
2026-09-21 14:09:24 +0530

    Automatic update

--- train.py
+++ train.py
@@ -12,4 +12,4 @@ X=pd.DataFrame(data.data, columns=data.feature_name)
 y=data.target
 print(f"Total records:{X.shape[0]}")
 
-X_train, X_test, y_train, y_test= train_test_split(X, y, )
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
