d307fc73b7b053119038cdbcde061f92fd6614da
Shraya Jaiswal <shrayajaiswal1@gmail.com>
2026-09-21 19:05:29 +0530

    Automatic update

--- train.py
+++ train.py
@@ -8,8 +8,20 @@ import joblib
 print("Loading datasets")
 data=fetch_california_housing()
 
-X=pd.DataFrame(data.data, columns=data.feature_name)
+X=pd.DataFrame(data.data, columns=data.feature_names)
 y=data.target
 print(f"Total records:{X.shape[0]}")
 
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=42)
+
+#training a model
+model=RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+model.fit(X_train, y_train)
+y_pred= model.predict(X_test)
+mae= mean_absolute_error(y_test, y_pred)
+r2=r2_score(y_test, y_pred)
+print(f"average mean error: ${mae*100000:,.0f}")
+
