7526505d45e3a4e4d66a5d017ca8308418002e0e
Shraya Jaiswal <shrayajaiswal1@gmail.com>
2026-09-21 14:03:06 +0530

    Automatic update

---
+++ train.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, r2_score
+import pandas as pd
+import joblib
+
